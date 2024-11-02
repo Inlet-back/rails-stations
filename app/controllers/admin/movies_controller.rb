edab60c6daee0312c588ca362eb4e8d3b334424f
Ura <mikeneko41@icloudd.com>
2024-11-02 17:31:24 +0900

    admin/postコントローラー作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,22 @@ class Admin::MoviesController < ApplicationController
   def index 
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      flash[:error] = @movie.errors
+      render :new
+    end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:name, :description, :year, :image_url)
+  end
 end
